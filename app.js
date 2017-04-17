709b70cb73574e6e94df6adc28278dec2d3ed128
Helen Yau <helenyau13@gmail.com>
2017-04-17 14:25:35 -0700

    link javascript and css to pug

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use('/static', express.static('public'))
+
 
 app.get('/', function (req, res) {
   res.render('index')
