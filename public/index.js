2206bbc3f3d24fc9c138ab95baef4c9f0b6c4a92
bj bautista <djbrigidope@bjs-MacBook-Pro.local>
2017-04-17 17:54:43 -0700

    worked populating list items

--- public/index.js
+++ public/index.js
@@ -1,1 +1,27 @@
-w3IncludeHTML();
+function addButton (){
+  var nameFile = prompt('name your mark down file');
+  var node =document.createElement('li');
+  var textNode = document.createTextNode(nameFile);
+  node.appendChild(textNode);
+  document.getElementById('navi').appendChild(node);
+
+  $('#navi li').attr('id', function(i){
+    var idOfLi = 'id'+(i+1);
+    return idOfLi;
+    var element=document.getElementById(idOfLi);
+    element.addEventListener('click', function() {
+      var header = document.getElementById('header').innerHTML = 'puppies';
+    })
+
+  $("navi li").each(function() {
+           $(this).attr("class", "mdFile");
+     });
+  })
+}
+
+
+// function copyListName () {
+//   $('#pages li').attr('id', function(i){
+//     return 'page'+(i+1);
+//   })
+// }
